5880fb7cb52d930a4bee1f2c389d4f41d89a9062
RafaZeero <rafaa99@gmail.com>
2023-09-16 16:31:09 -0300

    graceful shutdown

--- main.go
+++ main.go
@@ -4,12 +4,18 @@ import (
 	"context"
 	"fmt"
 	"orders-api/application"
+	"os"
+	"os/signal"
 )
 
 func main() {
 	app := application.New()
 
-	err := app.Start(context.TODO())
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+
+	defer cancel()
+
+	err := app.Start(ctx)
 	if err != nil {
 		fmt.Println("Failed to start app:", err)
 	}
